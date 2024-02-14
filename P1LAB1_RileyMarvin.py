6d07d0552a66e9d151117c45a9457a2efe206503
RileyMarv <160041507+RileyMarv@users.noreply.github.com>
2024-02-14 10:26:36 -0500

    Add files via upload

---
+++ P1LAB1_RileyMarvin.py
@@ -0,0 +1,14 @@
+#Marvin Riley
+
+#2024-02-12
+
+#P1LAB1
+
+#Using input and output
+
+#create a variable to hold first name
+first_name = input("Enter youer first name: ")
+
+#print first_name with greeting
+
+print ("Hey", first_name, "\nWelcome to zyBooks!")
